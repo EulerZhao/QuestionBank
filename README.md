1d97b99ee0076885d86816d9e3ef5b6c4a8ef243
Seair <1905517207@qq.com>
2019-05-16 21:57:24 +0800

    online search

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 本仓库致力于收集四川大学体育理论考试题目  
 
-地址：[码云](https://gitee.com/cherry-l/QuestionBank) or [Github](https://github.com/s8cm/QuestionBank)
+地址：<a href="https://gitee.com/cherry-l/QuestionBank" target="_blank">码云</a> or <a href="https://github.com/s8cm/QuestionBank" target="_blank">Github</a>
 
 最后更新时间: 2019-05-16 01:29:29  
 
@@ -20,22 +20,24 @@
 
 题库 | 大小 | 行数 | 题数
 -|-|-|-
-2019-05-16 21:49:58  
+2019-05-16 21:57:02  
 
 
 * * *
 
 ### 使用帮助
 
-可将题库下载到本地，PC端用记事本，安卓端推荐用[MT管理器](https://www.coolapk.com/apk/bin.mt.plus)，酱可以边做题目边搜索  
+可将题库下载到本地，PC端用记事本，安卓端推荐用<a href="https://www.coolapk.com/apk/bin.mt.plus" target="_blank">MT管理器</a>  
 
 本地搜索是最快的，当然也提供网页搜索：
 
-[在线搜索-1](http://i.s8cm.cn/) or [在线搜索-2](http://im.s8cm.cn/)
+<a href="http://i.s8cm.cn/" target="_blank">i.s8cm.cn</a>
+ or 
+<a href="http://im.s8cm.cn/" target="_blank">im.s8cm.cn</a>
 
 ### 联系方式
 
-QQ [1328357049](http://wpa.qq.com/msgrd?v=3&uin=1328357049&site=qq&menu=yes)  
+QQ <a href="http://wpa.qq.com/msgrd?v=3&uin=1328357049&site=qq&menu=yes" target="_blank">1328357049</a>  
 
 ### 致谢
 
--- k.py
+++ k.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # @Author: seair
 # @Date:   2019-05-14 22:46:00
-# @Last Modified by:   seair
-# @Last Modified time: 2019-05-16 21:49:51
+# @Last Modified by:   Seair
+# @Last Modified time: 2019-05-16 21:56:56
 
 import os
 import time
@@ -123,7 +123,7 @@ README = '''# QuestionBank
 
 本仓库致力于收集四川大学体育理论考试题目  
 
-地址：[码云](https://gitee.com/cherry-l/QuestionBank) or [Github](https://github.com/s8cm/QuestionBank)
+地址：<a href="https://gitee.com/cherry-l/QuestionBank" target="_blank">码云</a> or <a href="https://github.com/s8cm/QuestionBank" target="_blank">Github</a>
 
 最后更新时间: 2019-05-16 01:29:29  
 
@@ -144,15 +144,17 @@ README = '''# QuestionBank
 
 ### 使用帮助
 
-可将题库下载到本地，PC端用记事本，安卓端推荐用[MT管理器](https://www.coolapk.com/apk/bin.mt.plus)，酱可以边做题目边搜索  
+可将题库下载到本地，PC端用记事本，安卓端推荐用<a href="https://www.coolapk.com/apk/bin.mt.plus" target="_blank">MT管理器</a>  
 
 本地搜索是最快的，当然也提供网页搜索：
 
-[在线搜索-1](http://i.s8cm.cn/) or [在线搜索-2](http://im.s8cm.cn/)
+<a href="http://i.s8cm.cn/" target="_blank">i.s8cm.cn</a>
+ or 
+<a href="http://im.s8cm.cn/" target="_blank">im.s8cm.cn</a>
 
 ### 联系方式
 
-QQ [1328357049](http://wpa.qq.com/msgrd?v=3&uin=1328357049&site=qq&menu=yes)  
+QQ <a href="http://wpa.qq.com/msgrd?v=3&uin=1328357049&site=qq&menu=yes" target="_blank">1328357049</a>  
 
 ### 致谢
 
